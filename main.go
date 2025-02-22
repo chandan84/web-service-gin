c25d8f05657f28a9a0ea4aff64625370ee7a9936
chandan84 <chandan.mishra.se@gmail.com>
2025-02-21 20:05:14 -0500

    added movies functions

--- main.go
+++ main.go
@@ -13,11 +13,62 @@ type album struct {
     Price  float64 `json:"price"`
 }
 
+type movie struct {
+    ID            string  `json:"id"`
+    Title         string  `json:"title"`
+    Genre         string  `json:"genre"`
+    ReleasedDate  string  `json:"releaseDate"`
+    Director      string  `json:"director"`
+}
+
 // albums slice to seed record album data.
 var albums = []album{
     {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
     {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
     {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+var movies = []movie{
+    {ID: "1", Title: "Catch me if you can", Genre: "Comedy", ReleasedDate: "2002/02/14", Director: "Steven Spielberg"},
+    {ID: "2", Title: "Terminator", Genre: "Action", ReleasedDate: "1985/06/01", Director: "James Cameron"},
+    {ID: "3", Title: "Terminator: Judgement Day", Genre: "Action", ReleasedDate: "1996/10/01", Director: "James Cameron"},
+    {ID: "4", Title: "Avataar", Genre: "Action/Drama", ReleasedDate: "2009/08/01", Director: "James Cameron"},
+}
+
+//getMovies responds with the list of all movies as JSON
+func getMovies(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, movies)
+}
+
+// postMovies adds an album from JSON received in the request body.
+func postMovies(c *gin.Context) {
+    var newMovie movie
+
+    // Call BindJSON to bind the received JSON to
+    // newMovie.
+    if err := c.BindJSON(&newMovie); err != nil {
+        return
+    }
+
+    // Add the new album to the slice.
+    movies = append(movies, newMovie)
+    c.IndentedJSON(http.StatusCreated, newMovie)
+}
+
+// getMovieByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a response.
+func getMovieByID(c *gin.Context) {
+    id := c.Param("id")
+
+    // Loop over the list of movies, looking for
+    // an movie whose ID value matches the parameter.
+    for _, a := range movies {
+        if a.ID == id {
+            c.IndentedJSON(http.StatusOK, a)
+            return
+        }
+    }
+    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "movie not found"})
 }
 
 // getAlbums responds with the list of all albums as JSON.
@@ -61,6 +112,10 @@ func main() {
     router.GET("/albums", getAlbums)
     router.GET("/albums/:id", getAlbumByID)
     router.POST("/albums", postAlbums)
+
+    router.GET("/movies", getMovies)
+    router.GET("/movies/:id", getMovieByID)
+    router.POST("/movies", postMovies)
 
     router.Run("localhost:8080")
 }
